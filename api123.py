4276bd766595b697e7318b506628a752386d8f05
Niranjan <niranjankhedkar23@gmail.com>
2023-04-18 20:33:18 +0530

    iubf123

--- api123.py
+++ api123.py
@@ -11,29 +11,41 @@ def leaf_info():
         data = request.get_json()
         print(data)
         scientific_name=data['scientific_name']
+    if not scientific_name:
+        return jsonify({'error': 'Scientific name parameter is required.'}), 400
     print(scientific_name)
-    url1 = f'https://www.google.com/search?q={scientific_name}'
-    url2 = f'https://www.google.com/search?q=medicinal+properties+of+{scientific_name}'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
-    
-    response1 = requests.get(url1,headers=headers)
-    response2 = requests.get(url2, headers=headers)
-    soup1 = BeautifulSoup(response1.text, 'html.parser')
-    soup2= BeautifulSoup(response2.text, 'html.parser')
-    # search_result = soup2.find('div', class_='g')
-    common_name = soup1.find('span', class_='yKMVIe').text.strip()
-    
-    desc = soup1.find('div', class_='kno-rdesc')
-    description = desc.find('span').text.strip()
-    medicinal_properties = soup2.find('span', class_='hgKElc').text.strip()
-    data = {
-        'common_name': common_name,
-        'description': description,
-        'medicinal_properties': medicinal_properties
-    }
-    return jsonify(data)
+    try:
+        url1 = f'https://www.google.com/search?q={scientific_name}'
+        url2 = f'https://www.google.com/search?q=medicinal+properties+of+{scientific_name}'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        }
+        
+        response1 = requests.get(url1,headers=headers)
+        response2 = requests.get(url2, headers=headers)
+        soup1 = BeautifulSoup(response1.text, 'html.parser')
+        soup2= BeautifulSoup(response2.text, 'html.parser')
+        # search_result = soup2.find('div', class_='g')
+        common_name = soup1.find('span', class_='yKMVIe').text.strip()
+        
+        desc = soup1.find('div', class_='kno-rdesc')
+        description = desc.find('span').text.strip()
+        medicinal_properties = soup2.find('span', class_='hgKElc').text.strip()
+        data = {
+            'common_name': common_name,
+            'description': description,
+            'medicinal_properties': medicinal_properties
+        }
+        return jsonify(data), 200
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': f'Request error: {e}'}), 500
+
+    except AttributeError as e:
+        return jsonify({'error': f'Attribute error: {e}'}), 500
+
+    except Exception as e:
+        return jsonify({'error': f'Unknown error: {e}'}), 500
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True,port=5000)
+    app.run(debug=True)
